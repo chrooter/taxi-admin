78f664a80e8fd9c54c2fee83bbf5aaa72bb5f624
graf.abolmasov <graf.abolmasov@gmail.com>
2011-10-12 04:36:31 +0000

    Added some servlets

---
+++ v02/src/ru/dreamjteam/servlets/CreateOrderServlet.java
@@ -0,0 +1,10 @@
+package ru.dreamjteam.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author abolmasov (11.10.2011 10:16:13)
+ * @version $Revision$
+ */
+public class CreateOrderServlet extends HttpServlet {
+}
