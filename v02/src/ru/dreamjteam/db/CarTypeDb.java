2f1359ba26b1f7debbcb7848301c1e16097ca5a9
DimanSergiyenko <DimanSergiyenko@gmail.com>
2011-10-22 10:08:42 +0000

    1

--- v02/src/ru/dreamjteam/db/CarTypeDb.java
+++ v02/src/ru/dreamjteam/db/CarTypeDb.java
@@ -28,10 +28,10 @@ public class CarTypeDb {
     public static void insert ( CarType row) throws DbAccessException {
         
         try {
-            Connection conn = getConnection();
+            
+            Connection conn = Connect.GetConnect();
 
-      	String query = "INSERT INTO TYPES VALUES (TYPES_SEQ.nextval, ?, ?, ?, ?)";
-           // String query = "INSERT INTO TYPES VALUES (CAR_TYPE_SEQ.nextval, ?, ?, ?, ?)";
+            String query = "INSERT INTO TYPES VALUES (TYPES_SEQ.nextval, ?, ?, ?, ?)";
             PreparedStatement ps = conn.prepareStatement(query);
             
             ps.setString(1, row.getName());
@@ -42,6 +42,7 @@ public class CarTypeDb {
             ps.executeUpdate();
             ps.close();
             conn.close();
+
         }
         catch (SQLException ex) {
             throw new DbAccessException(ex);
@@ -91,39 +92,40 @@ public class CarTypeDb {
         return select ("TYPE_ID", ct);
     }
                       
-    public static String select (String orderBy, CarType ct) throws DbAccessException {
+   public static String select (String orderBy, CarType ct) throws DbAccessException {
         try {
             int conditions = 1;
-            Connection conn = getConnection();
+            Connection conn = Connect.GetConnect();
+
             String query = "SELECT * FROM TYPES ";
             
             if (ct != null) {
                 query += "WHERE TYPE_ID = TYPE_ID ";
-                if (ct.getId() != null) 
+                if (ct.getId() != 0) 
                     query += "AND TYPE_ID = ? ";
                 if (ct.getName() != null) 
                     query += "AND NAME LIKE ? ";
-                if (ct.getSeatCap() != null) 
+                if (ct.getSeatCap() != null)
                     query += "AND SEATING_CAPACITY = ? ";
-                if (ct.getMassCap() != null) 
+                if (ct.getMassCap() != null)
                     query += "AND CAPACITY = ? ";
-                if (ct.getCostPerKm() != null) 
+                if (ct.getCostPerKm() != null)
                     query += "AND COST_PER_KM = ? ";
             }    
             query += "ORDER BY " + orderBy;
 
             PreparedStatement ps = conn.prepareStatement(query);
-            ps.toString();
+
             if (ct != null) {
-                if (ct.getId() != null) 
+                if (ct.getId() != 0) 
                     ps.setInt(conditions++, ct.getId());
                 if (ct.getName() != null)
                     ps.setString(conditions++, "%"+ct.getName()+"%");
                 if (ct.getSeatCap() != null)
                     ps.setInt(conditions++, ct.getSeatCap());
-                if (ct.getMassCap() != null)
+                if (ct.getMassCap() !=null)
                     ps.setInt(conditions++, ct.getMassCap());
-                if (ct.getCostPerKm() != null) 
+                if (ct.getCostPerKm() != null)
                     ps.setInt(conditions++, ct.getCostPerKm());
             }
             
@@ -138,7 +140,6 @@ public class CarTypeDb {
                 row.setSeatCap(rs.getInt(3));
                 row.setMassCap(rs.getInt(4));
                 row.setCostPerKm(rs.getInt(5));
-                row.setCars(new Cars());
                 rows.getCarType().add(row);
             }
 
@@ -156,42 +157,27 @@ public class CarTypeDb {
             //return null;
         }
     }
- 
+    
     public static void update (CarType row) throws DbAccessException {
         try {
-            int conditions = 1;
-            Connection conn = getConnection();            
+            Connection conn = Connect.GetConnect();
+
+            String query = "UPDATE TYPES "
+                    + "SET NAME = ?, SEATING_CAPACITY = ?, CAPACITY = ?, COST_PER_KM = ? "
+                    + "WHERE TYPE_ID = ?";
             
-            if ((row != null) && (row.getId() != null)) {
-                String query = "UPDATE TYPES SET TYPE_ID = TYPE_ID ";
-                if (row.getName() != null)
-                    query += ", NAME = ? ";
-                if (row.getSeatCap() != null)
-                    query += ", SEATING_CAPACITY = ? ";
-                if (row.getMassCap() != null)
-                    query += ", CAPACITY = ? ";
-                if (row.getCostPerKm() != null)
-                    query += ", COST_PER_KM = ? ";
-                
-                query += "WHERE TYPE_ID = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
             
-                PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, row.getName());
+            ps.setInt(2, row.getSeatCap());
+            ps.setInt(3, row.getMassCap());
+            ps.setInt(4, row.getCostPerKm());
+            ps.setInt(5, row.getId());
             
-                if (row.getName() != null)
-                    ps.setString(conditions++, row.getName());
-                if (row.getSeatCap() != null)
-                    ps.setInt(conditions++, row.getSeatCap());
-                if (row.getMassCap() != null)
-                    ps.setInt(conditions++, row.getMassCap());
-                if (row.getCostPerKm() != null)
-                    ps.setInt(conditions++, row.getCostPerKm());
-                ps.setInt(conditions, row.getId());
+            ps.executeUpdate();
             
-                ResultSet rs = ps.executeQuery();
- 
-                rs.close();
-                conn.close();
-            }
+            conn.close();
+
         
         } catch (NamingException ex) {
             throw new DbAccessException(ex);
@@ -203,4 +189,5 @@ public class CarTypeDb {
 
         }
     }
-}
+    
+ }
